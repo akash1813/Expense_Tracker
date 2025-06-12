3bbe09a6d3b3bc3db4cde1870799717b64064570
Akash Dhiman <akashdhiman@Mac.lan>
2025-06-13 01:20:29 +0530

    Income page overview done

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboardLayout from '../../components/Layouts/DashboardLayout'
+import IncomeOverview from '../../components/Income/IncomeOverview';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useFetcher } from 'react-router-dom';
+import Modal from '../../components/Modal';
 
-function Income() {
+const Income = () => {
+
+  const [incomeData, setIncomeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+
+  });
+
+  const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+
+  const fetchIncomeDetails = async () => {
+    if (loading)
+      return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(`${API_PATHS.INCOME.GET_ALL_INCOME}`);
+
+      if (response.data) {
+        setIncomeData(response.data);
+      }
+    }
+    catch (error) {
+      console.log("Something went wrong. Please try again", error);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  const handleAddIncome = async (income) => {
+
+  }
+
+  const deleteIncome = async (id) => {
+
+  }
+
+  const handleDownloadIncomeDetails = async () => {
+
+  }
+
+  useEffect(() => {
+    fetchIncomeDetails();
+
+    return () => { }
+  }, [])
+
   return (
-    <div>Income</div>
+    <DashboardLayout activeMenu="Income">
+      <div className='my-5 mx-auto'>
+        <div className='grid grid-cols-1 gap-6'>
+          <div className=''>
+            <IncomeOverview
+              transactions={incomeData}
+              onAddIncome={() => setOpenAddIncomeModal(true)}
+
+            />
+          </div>
+
+        </div>
+
+        <Modal
+          isOpen={openAddIncomeModal}
+          onClose={() => setOpenAddIncomeModal(false)}
+          title="Add Income"
+        >
+          <div>Add Income Form</div>
+        </Modal>
+
+      </div>
+    </DashboardLayout>
   )
 }
 
-export default Income
+export default Income;
