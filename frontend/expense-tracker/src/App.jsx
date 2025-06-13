f0b9a0ac3a53ffd53228ff098da449302fb8fb6e
Akash Dhiman <akashdhiman@Mac.lan>
2025-06-13 18:15:27 +0530

    Income page add income,income overview, income list, delete income compeleted

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -12,24 +12,36 @@ import Home from './pages/Dashboard/Home'
 import Expense from './pages/Dashboard/Expense'
 import Income from './pages/Dashboard/Income'
 import UserProvider from './context/userContext'
+import {Toaster} from "react-hot-toast";
 
 function App() {
   return (
     <UserProvider>
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root />}  />
-          <Route path="/login" element={<Login />} />
-         <Route path="/signup" element={<SignUp />} />
-         <Route path="/dashboard" element={<Home />} />
-         <Route path="/expense" element={<Expense />} />
-         <Route path="/income" element={<Income />} />
-         
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/expense" element={<Expense />} />
+            <Route path="/income" element={<Income />} />
 
-        </Routes>
-      </Router>
-    </div>
+
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOptions={{
+          className:" ",
+          style:{
+            fontSize:'13px'
+          },
+        }}
+
+      />
+
     </UserProvider>
   )
 }
@@ -42,8 +54,8 @@ const Root = () => {
   //Check if token exists in localStorage
   const isAuthenticated = !!localStorage.getItem('token')
 
-  return isAuthenticated ?(
-    <Navigate to= "/dashboard" />
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
   ) : (
     <Navigate to="/login" />
   )
--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -5,6 +5,11 @@ import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { useFetcher } from 'react-router-dom';
 import Modal from '../../components/Modal';
+import AddIncomeForm from '../../components/Income/AddIncomeForm';
+import toast from 'react-hot-toast';
+import IncomeList from '../../components/Income/IncomeList';
+import DeleteAlert from '../../components/Layouts/DeleteAlert';
+
 
 const Income = () => {
 
@@ -40,11 +45,59 @@ const Income = () => {
   }
 
   const handleAddIncome = async (income) => {
+      const {source,amount,date,icon} = income;
+      
+      //Validation check
+      if(!source.trim()){
+        toast.error("Source is required");
+        return;
+      }
+
+      if(!amount || isNaN(amount) || Number(amount) <=0 ){
+        toast.error("Amount should be a valid number greater than 0");
+        return;
+      }
+
+      if(!date){
+        toast.error("Date is required");
+        return;
+      }
+
+      try{
+        await axiosInstance.post(API_PATHS.INCOME.ADD_INCOME, {
+          source,
+          amount,
+          date,
+          icon,
+
+        });
+
+        setOpenAddIncomeModal(false);
+        toast.success("Income Added Successfully");
+        fetchIncomeDetails();
+
+      }
+      catch(error){
+        console.error("Error adding income",error.response?.data?.message || error.message);
+      }
+
+
+
 
   }
 
   const deleteIncome = async (id) => {
 
+    try{
+      await axiosInstance.delete(API_PATHS.INCOME.DELETE_INCOME(id));
+
+      setOpenDeleteAlert({show:false,data:null});
+      toast.success("Income details deleted successfully");
+      fetchIncomeDetails();
+    }
+    catch(error){
+      console.error("Error deleting income: ",error.response?.data?.message  || error.message);
+    }
   }
 
   const handleDownloadIncomeDetails = async () => {
@@ -69,6 +122,14 @@ const Income = () => {
             />
           </div>
 
+          <IncomeList
+            transactions={incomeData}
+            onDelete={(id) => {
+              setOpenDeleteAlert({show:true,data:id})
+            }}
+            onDownload={handleDownloadIncomeDetails}
+          />
+
         </div>
 
         <Modal
@@ -76,7 +137,18 @@ const Income = () => {
           onClose={() => setOpenAddIncomeModal(false)}
           title="Add Income"
         >
-          <div>Add Income Form</div>
+          <AddIncomeForm onAddIncome={handleAddIncome } />
+        </Modal>
+
+        <Modal
+          isOpen={openDeleteAlert.show}
+          onClose={() => setOpenDeleteAlert({show:false, data:null})}
+          title="Delete Income"
+        >
+            <DeleteAlert 
+            content="Are you sure you want to delete this income details? "
+            onDelete={() => deleteIncome(openDeleteAlert.data)}
+            />
         </Modal>
 
       </div>
