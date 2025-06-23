18ab60a4c216ab2ee4851b0b853cbceb05719942
Akash Dhiman <akashdhiman@Mac.lan>
2025-06-23 18:11:56 +0530

    Fix Vite configuration and build settings

--- frontend/expense-tracker/vite.config.js
+++ frontend/expense-tracker/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build' // ← check if this is there
+    outDir: 'build'
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
